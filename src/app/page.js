32285fea667435739abad16c462035c0e90d7601
boyming0619-create <boyming0619@gmail.com>
2026-01-13 18:18:41 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
-// import Wizard from "@/components/models/Wizard";
 import Navigation from "@/components/navigation";
-
 import dynamic from "next/dynamic";
-const Wizard = dynamic(() => import("@/components/models/Wizard"), {
-  ssr: false,
-});
+
+const Wizard = dynamic(() => import("@/components/models/Wizard"), { ssr: false });
 
 export default function Home() {
   return (
@@ -21,26 +18,11 @@ export default function Home() {
         className="-z-50 w-full h-full object-cover object-center opacity-50"
       />
 
-      <div className="w-full h-screen">
-       {/* 找到 src/app/page.js 里的这段并修改 */}
-<div className="w-full h-screen">
-  <Navigation />
-  <RenderModel>
-    {/* 电脑端显示 3D 巫师 */}
-    <div className="hidden md:block h-full w-full">
-      <Wizard />
-    </div>
-    
-    {/* 手机端显示静态图片，防止崩溃 */}
-    <div className="block md:hidden flex items-center justify-center h-full w-full">
-      <img 
-        src="/background/home-background.png" // 这里可以换成你自己的 PNG 角色图
-        alt="Character" 
-        className="w-[70%] h-auto object-contain opacity-80" 
-      />
-    </div>
-  </RenderModel>
-</div>
+      <div className="w-full h-screen relative">
+        <Navigation />
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
       </div>
     </main>
   );

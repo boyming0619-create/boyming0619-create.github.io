d26b59b031983e386a40312c44eba7a282f9b654
boyming0619-create <boyming0619@gmail.com>
2026-01-13 16:43:44 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',};
+const nextConfig = {output: 'export',
+                    images: {
+                      unoptimized: true, // 必须加这一行，否则在 GitHub Pages 上图片会加载失败
+  },
+};
 
 export default nextConfig;
